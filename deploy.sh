6e6dd2d8ffaa02069e2e65e438f4473a78260c26
Daniel Weigel <daniel.wgel@gmail.com>
2022-01-02 14:03:19 +1300

    editing the -e CI=true flag to the run test cmd

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ docker push danieldockerid2021/multi-client:$SHA
 docker push danieldockerid2021/multi-server:$SHA
 docker push danieldockerid2021/multi-worker:$SHA
 
-kubectl apply -f ./k8s
+kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=danieldockerid2021/multi-server:$SHA
 kubectl set image deployments/client-deployment client=danieldockerid2021/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=danieldockerid2021/multi-worker:$SHA
